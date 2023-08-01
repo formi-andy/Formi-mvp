6613129687df2d8f0e4094aedd1d8d6177f1b89a
Jerry Zhou <8799269+jzhou891@users.noreply.github.com>
2023-08-01 10:21:04 -0700

    form width

--- packages/client/src/app/login/page.tsx
+++ packages/client/src/app/login/page.tsx
@@ -7,7 +7,7 @@ export default function Login() {
   return (
     <div className="flex flex-1 justify-center items-center flex-col w-full">
       <p className="text-3xl font-semibold pb-8">Log in to Homescope</p>
-      <div className="flex w-full max-w-sm mx-auto overflow-hidden bg-white rounded-lg shadow-lg dark:bg-zinc-800 lg:max-w-4xl">
+      <div className="flex w-fit min-w-[400px] max-w-sm mx-auto overflow-hidden bg-white rounded-lg shadow-lg dark:bg-zinc-800 lg:max-w-4xl">
         <div className="flex flex-col items-center gap-y-4 w-full max-w-sm p-6 m-auto mx-auto">
           <div className="flex justify-center mx-auto">
             <Image width={40} height={40} src={"/assets/logo.svg"} alt="logo" />
