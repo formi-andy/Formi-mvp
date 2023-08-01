f5f5aaba0748de75a1061406dddbf95e299ca45e
Jerry Zhou <8799269+jzhou891@users.noreply.github.com>
2023-07-31 17:59:04 -0700

    add favicons

--- packages/client/src/app/layout.tsx
+++ packages/client/src/app/layout.tsx
@@ -16,6 +16,14 @@ export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
       <head>
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+        <link
+          rel="apple-touch-icon"
+          href="/apple-touch-icon.png"
+          type="image/png"
+          sizes="180x180"
+        />
+        
         <ColorSchemeScript />
       </head>
       <body className="h-fit">
