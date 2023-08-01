be9898ea7fca78699b7f78ea024adb8576ec0a07
Jerry Zhou <8799269+jzhou891@users.noreply.github.com>
2023-08-01 16:04:14 -0700

    move signature logic to server

---
+++ packages/client/src/app/api/auth/video-signature/route.ts
@@ -0,0 +1,32 @@
+import { generateVideoToken } from "@/utils/util";
+
+type Payload = {
+  meetingId: string;
+  password: string;
+  sessionKey: string;
+  userIdentity: string;
+  cloudRecordingOption: string;
+  cloudRecordingElection: string;
+  role: string;
+};
+
+export async function POST(request: Request) {
+  // get user role from middleware
+  const requestData: Payload = await request.json();
+
+  let signature = generateVideoToken(
+    process.env.ZOOM_MEETING_SDK_KEY || "",
+    process.env.ZOOM_MEETING_SDK_SECRET || "",
+    requestData.meetingId,
+    requestData.password,
+    requestData.sessionKey,
+    requestData.userIdentity,
+    parseInt(requestData.role || "1", 10),
+    requestData.cloudRecordingOption,
+    requestData.cloudRecordingElection
+  );
+
+  return new Response(JSON.stringify({ signature }), {
+    headers: { "Content-Type": "application/json" },
+  });
+}
