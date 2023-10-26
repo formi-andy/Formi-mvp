4b95d10de6cae33fe43f42101835cc11b3e0f4b1
jameszhou02 <jameszhou02@gmail.com>
2023-10-26 11:38:09 -0700

    feat: added notifs to dropzone
    - disabled duplicate file addition bug
    - added nested tailwind css to postcss config to remove warning
    - removed textinput for file renaming in accepted files

--- packages/client/components/Tutorials/PlatformTutorial.tsx
+++ packages/client/components/Tutorials/PlatformTutorial.tsx
@@ -1,12 +1,30 @@
 "use client";
 
 import { useState } from "react";
+import { useQuery } from "convex/react";
+import { api } from "@/convex/_generated/api";
+
+import AppLoader from "@/components/Loaders/AppLoader";
+import NotFoundPage from "@/app/not-found";
 import PlatformTutorialModal from "./PlatformTutorialModal";
 
 const PlatformTutorial = () => {
   const [opened, setOpened] = useState(true);
+  const user = useQuery(api.users.currentUser);
 
-  return <PlatformTutorialModal opened={opened} setOpened={setOpened} />;
+  if (user === undefined) {
+    return <AppLoader />;
+  }
+
+  if (!user) {
+    return <NotFoundPage />;
+  }
+
+  if (user.role === "patient") {
+    return <PlatformTutorialModal opened={opened} setOpened={setOpened} />;
+  } // else return platform tutorial for doctors
+
+  return <></>;
 };
 
 export default PlatformTutorial;
