df759a76db3c582793ee25d63f7325ef978ea8b6
Jerry Zhou <8799269+jzhou891@users.noreply.github.com>
2023-12-07 23:55:53 -0500

    feat: Change password

--- packages/client/app/(public)/api/check-password/route.ts
+++ packages/client/app/(public)/api/check-password/route.ts
@@ -7,7 +7,7 @@ export async function POST(request: Request) {
 
   return new Response(
     JSON.stringify({
-      verified: requestData.password === "WelcomeToFormiHealth",
+      verified: requestData.password === "Homescope",
     }),
     {
       headers: { "Content-Type": "application/json" },
