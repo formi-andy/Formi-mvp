b9f4dca173ff2e12632b3ca1250434ac73b5f36c
Jerry Zhou <8799269+jzhou891@users.noreply.github.com>
2024-02-13 00:41:05 -0500

    fix: Fix sidebar button

--- packages/client/components/Chat/sidebar-mobile.tsx
+++ packages/client/components/Chat/sidebar-mobile.tsx
@@ -19,9 +19,9 @@ interface SidebarMobileProps {
 export function SidebarMobile({ children }: SidebarMobileProps) {
   return (
     <Sheet>
-      <SheetTrigger asChild>
-        <Tooltip>
-          <TooltipTrigger asChild>
+      <Tooltip>
+        <TooltipTrigger asChild>
+          <SheetTrigger asChild>
             <Button
               variant="ghost"
               className="ml-2 top-1/2 absolute flex size-9 p-0 lg:hidden"
@@ -29,12 +29,12 @@ export function SidebarMobile({ children }: SidebarMobileProps) {
               <BsChevronCompactRight className="size-6" />
               <span className="sr-only">Open Sidebar</span>
             </Button>
-          </TooltipTrigger>
-          <TooltipContent className="flex items-center justify-center absolute left-8 top-1 w-28">
-            Open Sidebar
-          </TooltipContent>
-        </Tooltip>
-      </SheetTrigger>
+          </SheetTrigger>
+        </TooltipTrigger>
+        <TooltipContent className="flex items-center justify-center absolute left-8 top-1 w-28">
+          Open Sidebar
+        </TooltipContent>
+      </Tooltip>
       <SheetContent className="flex top-16 h-[calc(100%_-_64px)] w-[300px] flex-col p-0 pt-8">
         <Sidebar className="flex">{children}</Sidebar>
       </SheetContent>
