cc75128aab069156f068fe29125899d74db55006
James Zhou <jameszhou02@gmail.com>
2024-02-13 22:08:50 -0800

    fix theme

--- packages/client/components/Chat/sidebar-list.tsx
+++ packages/client/components/Chat/sidebar-list.tsx
@@ -28,9 +28,9 @@ export async function SidebarList({ userId }: SidebarListProps) {
           </div>
         )}
       </div>
-      <div className="flex items-center justify-between p-4">
+      {/* <div className="flex items-center justify-between p-4">
         <ThemeToggle />
-      </div>
+      </div> */}
     </div>
   );
 }
